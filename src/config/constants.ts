629db1c1fb648627401e1cd2c22833cedd087684
domakun <http://52.194.207.217>
2019-10-08 13:28:51 +0800

    update local master

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -137,6 +137,7 @@ export const defaultNetworkConfig = {
     gas2xemRate: 20000,   //  1xem=20000gas
     networkConfirmations: 10,
     defaultNetworkMosaic,
+    DEFAULT_LOCK_AMOUNT:10
 }
 
 export const networkConfig = {
