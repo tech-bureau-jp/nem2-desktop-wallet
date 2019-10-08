f6c932fb2e4e5832cc56f8a0042820e24e3fcc3f
domakun <http://52.194.207.217>
2019-10-08 13:29:42 +0800

    update local master

---
+++ src/core/model/formattedTransactions/accountMetadataTransaction.ts
@@ -0,0 +1,22 @@
+import {FormattedTransaction, AppState} from '@/core/model'
+import {getRelativeMosaicAmount} from '@/core/utils'
+import {Transaction} from 'nem2-sdk'
+import {Store} from 'vuex';
+
+export class FormattedAccountMetadataTransaction extends FormattedTransaction {
+    dialogDetailMap: any
+    icon: any
+
+    constructor(  tx: Transaction,
+                  store: Store<AppState>) {
+        super(tx, store)
+        const {networkCurrency} = store.state.account
+
+        this.dialogDetailMap = {
+            'transfer_type': this.txHeader.tag,
+            'fee': getRelativeMosaicAmount(tx.maxFee.compact(), networkCurrency.divisibility) + networkCurrency.ticker,
+            'block': this.txHeader.block,
+            'hash': this.txHeader.hash,
+        }
+    }
+}
